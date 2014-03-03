c9692b1611115cee8fd48fcdfd3e1afd7fc3daac
tsubakimoto_s <tkmbmbr@gmail.com>
2014-03-03 14:23:38 +0900

    コマンドライン引数の追加

--- df_for_windows/Program.cs
+++ df_for_windows/Program.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 
 namespace df_for_windows
 {
     class Program
     {
+        /// <summary>
+        /// ヘッダ
+        /// </summary>
+        private const string HEADER = "Drive Type     Total    Used    Free    % ValumeName";
+
+        /// <summary>
+        /// セパレータ
+        /// </summary>
+        private const string SEPARATOR = "----- ------ ------- ------- ------- ---- ----------";
+
         /// <summary>
         /// 準備済ドライブの出力フォーマット
         /// </summary>
@@ -22,9 +33,28 @@ namespace df_for_windows
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Output(Analyze());
+            // 動作タイプ
+            var type = args.Length == 0 ? "-c" : args[0];
 
-            Console.ReadLine();
+            // ドライブ情報解析
+            var analyzed = Analyze();
+
+            switch (type)
+            {
+                case "-c":
+                    OutputToConsole(analyzed);
+                    break;
+
+                case "-t":
+                    var path = GetPath(args);
+                    OutputToFile(analyzed, path);
+                    break;
+
+                default: new ArgumentException("無効な引数です。"); break;
+            }
+
+            //Console.Write("ドライブ情報の解析が完了しました。キー入力で終了します。");
+            //Console.ReadLine();
         }
 
         /// <summary>
@@ -75,18 +105,51 @@ namespace df_for_windows
         }
 
         /// <summary>
-        /// 出力します。
+        /// ドライブ情報をコンソールに出力します。
         /// </summary>
-        /// <param name="driveInfos">出力ドライブ情報</param>
-        private static void Output(string[] driveInfos)
+        /// <param name="driveInfos">ドライブ情報</param>
+        private static void OutputToConsole(string[] driveInfos)
         {
             // Header
-            Console.WriteLine("Drive Type     Total    Used    Free    % ValumeName");
-            Console.WriteLine("----- ------ ------- ------- ------- ---- ----------");
+            Console.WriteLine(HEADER);
+            Console.WriteLine(SEPARATOR);
 
             // Body
             for (var i = 0; i < driveInfos.Length; i++)
                 Console.WriteLine(driveInfos[i]);
+        }
+
+        /// <summary>
+        /// ドライブ情報をファイルに出力します。
+        /// </summary>
+        /// <param name="driveInfos">ドライブ情報</param>
+        /// <param name="path">出力ファイルパス</param>
+        private static void OutputToFile(string[] driveInfos, string path)
+        {
+            // フォルダの存在チェック＆作成
+            var fi = new FileInfo(path);
+            if (!fi.Directory.Exists) fi.Directory.Create();
+
+            using (var sw = new StreamWriter(path, false, Encoding.UTF8))
+            {
+                // HEADER
+                sw.WriteLine(HEADER);
+                sw.WriteLine(SEPARATOR);
+
+                // BODY
+                for (var i = 0; i < driveInfos.Length; i++)
+                    sw.WriteLine(driveInfos[i]);
+            }
+        }
+
+        /// <summary>
+        /// コマンドライン引数から出力パスを取得します。
+        /// </summary>
+        /// <param name="args">コマンドライン引数</param>
+        /// <returns>出力パス</returns>
+        private static string GetPath(string[] args)
+        {
+            return args.Length < 2 ? @"c:\temp\df.txt" : args[1];
         }
     }
 
